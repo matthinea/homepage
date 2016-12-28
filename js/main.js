8572ced354a8d368a0387753222aacbed3f040f3
“Matthew <matthew.hinea@gmail.com>
2016-12-28 00:37:05 -0800

    swap jquery scroll listener for waypoints

--- js/main.js
+++ js/main.js
@@ -1,37 +1,19 @@
 let $pt = $("#pt");
-let blur = 1;
-let scrollCount = 0;
-let lastTop = 0;
-$(window).scroll(function() {
-  if (blur > 9) {
-    blur = 9;
-  } else if (blur < 1) {
-    blur = 1;
-  }
-  let thisTop = $(this).scrollTop();
-  if(thisTop > lastTop) {
-    if (scrollCount % 5 === 0) {
+let scrollArea = document.getElementById('hero-scroll-area');
+
+for (let ii = 1; ii <= 10; ii++) {
+  let wp = new Waypoint({
+    element: scrollArea,
+    handler: function(direction) {
       $pt.css({
-         'filter'         : 'blur(' + blur + 'px)',
-         '-webkit-filter' : 'blur(' + blur + 'px)',
-         '-moz-filter'    : 'blur(' + blur + 'px)',
-         '-o-filter'      : 'blur(' + blur + 'px)',
-         '-ms-filter'     : 'blur(' + blur + 'px)'
+        'filter'         : 'blur(' + ii + 'px)',
+        '-webkit-filter' : 'blur(' + ii + 'px)',
+        '-moz-filter'    : 'blur(' + ii + 'px)',
+        '-o-filter'      : 'blur(' + ii + 'px)',
+        '-ms-filter'     : 'blur(' + ii + 'px)'
       });
-      blur += 1;
-    }
-    scrollCount++;
-  } else {
-    if (scrollCount % 2 === 0) {
-      blur -= 1;
-      $pt.css({
-        'filter'         : 'blur(' + blur + 'px)',
-        '-webkit-filter' : 'blur(' + blur + 'px)',
-        '-moz-filter'    : 'blur(' + blur + 'px)',
-        '-o-filter'      : 'blur(' + blur + 'px)',
-        '-ms-filter'     : 'blur(' + blur + 'px)'
-      });
-    }
-  }
-  lastTop = thisTop;
-})
+      console.log("down");
+    },
+    offset: (Waypoint.viewportHeight() / 25 * (ii + 1) + 300) * -1
+  })
+}
