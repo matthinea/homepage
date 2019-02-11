decd4e09f355a2acf05259a0f7a3aec77dbab4c9
Matthew Hinea <matthew.hinea@gmail.com>
2019-02-10 22:09:40 -0800

    consolidate list items; make arrow prompt bring users all the way down to the contact me section

--- js/main.js
+++ js/main.js
@@ -122,7 +122,7 @@ var frameNum = 1;
 
 $arrow.on('click', function(event) {
   $('html, body').animate({scrollTop: window.innerHeight * frameNum}, 1000, "easeOutCubic");
-  if(frameNum <= 2) {
+  if(frameNum <= 3) {
       $arrow.css({
       "transform": "translateY(" + (window.innerHeight * frameNum) + "px)"
     });
