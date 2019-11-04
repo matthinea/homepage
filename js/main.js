d7522e05458f3de510d325ba91338f7acc2367ed
Matthew Hinea <matthew.hinea@gmail.com>
2019-11-03 18:27:23 -0800

    Remove portfolio section JS.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 var $pt = $("#pt");
 var $pt_svg = $("#pt_svg")
 var $mouse = $("#mouse");
-var $portfolio = $(".portfolio");
 var scrollArea = document.getElementById('hero-scroll-area');
 
 var wp;
@@ -19,18 +18,6 @@ var _loop = function _loop() {
 for (var ii = 1; ii <= 12; ii++) {
   _loop();
 }
-
-$('.scroll-to-portfolio').on('mousedown', function(event) {
-  // event.preventDefault();
-  console.log($portfolio.offset().top - $portfolio.css('margin-top'));
-  window.scrollTo(0, $about.offset().top + $about.outerHeight(true));
-  if (event.currentTarget.innerHTML === 'browser games') {
-    $('#slick-slide02').click();
-  }
-})
-
-
-
 
 // waypoints
 var $iconsText = $('.intro__icons__text');
@@ -83,17 +70,6 @@ var aboutWp = new Waypoint({
 })
 
 
-var $portfolio = $(".portfolio");
-var portfolioWp = new Waypoint({ 
-  element: $portfolio,
-  handler: function(direction) {
-    $portfolio.addClass('showing');
-  }, 
-  offset: $portfolio.height() / 2 + 100
-})
-
-
-
 var $cta = $('.cta');
 var $ctaText = $('.cta-wrapper').children();
 var ctaWp = new Waypoint({ 
@@ -107,22 +83,12 @@ var ctaWp = new Waypoint({
 });
 
 
-$(document).ready(function(){
-  $('.portfolio').slick({
-    dots: true,
-    fade: false,
-    nextArrow: '.slick-next',
-    prevArrow: '.slick-prev',
-  });
-});
-
-
 var $arrow = $('.scroll-invitation');
 var frameNum = 1;
 
 $arrow.on('click', function(event) {
   $('html, body').animate({scrollTop: window.innerHeight * frameNum}, 1000, "easeOutCubic");
-  if(frameNum <= 3) {
+  if(frameNum < 3) {
       $arrow.css({
       "transform": "translateY(" + (window.innerHeight * frameNum) + "px)"
     });
